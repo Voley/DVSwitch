6290a50d7b3bd036236563d196b8d5170522b7ba
Dmitry Volevodz <dimavolevodz@gmail.com>
2014-10-08 22:17:58 +0400

    Added pods

---
+++ Source/DVSwitch.h
@@ -0,0 +1,36 @@
+//
+//  DVSwitch.h
+//  DVSwitcherExample
+//
+//  Created by Dmitry Volevodz on 08.10.14.
+//  Copyright (c) 2014 Dmitry Volevodz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DVSwitch : UIControl
+
+@property (strong, nonatomic) UIColor *backgroundColor; // defaults to gray
+@property (strong, nonatomic) UIColor *sliderColor; // defaults to white
+@property (strong, nonatomic) UIColor *labelTextColorInsideSlider; // defaults to black
+@property (strong, nonatomic) UIColor *labelTextColorOutsideSlider; // defaults to white
+@property (strong, nonatomic) UIFont *font;
+@property (nonatomic) CGFloat cornerRadius; // defaults to 12
+@property (nonatomic) CGFloat sliderOffset; // slider offset from background, top, bottom, left, right
+
++ (instancetype)switchWithStringsArray:(NSArray *)strings;
+- (instancetype)initWithStringsArray:(NSArray *)strings;
+
+- (void)forceSelectedIndex:(NSInteger)index animated:(BOOL)animated; // sets the index, also calls handler block
+
+// This method sets handler block that is getting called after the switcher is done animating the transition
+
+- (void)setPressedHandler:(void (^)(NSUInteger index))handler;
+
+
+// This method sets handler block that is getting called right before the switcher starts animating the transition
+
+- (void)setWillBePressedHandler:(void (^)(NSUInteger index))handler;
+
+
+@end
